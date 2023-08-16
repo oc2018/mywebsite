f393037b9c791307eb669a83ec2a39309db13527
oc2018 <ontime.c23@gmail.com>
2023-08-16 12:23:33 +0300

    modified:   admin/src/services/messages.js 	modified:   admin/src/util/backendUrl.js

--- admin/src/services/messages.js
+++ admin/src/services/messages.js
@@ -4,7 +4,7 @@ import { baseURL } from '../util/backendUrl';
 export const messagesApi = createApi({
     reducerPath: 'messagesApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://mywebsite-backend-fe.vercel.app/api'
+        baseUrl: baseURL,
     }),
     endpoints: (builder) => ({
         getMessage: builder.query({
