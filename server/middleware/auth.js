0b677be31c3d98f1d7a5883f27346f462cb8cbee
Eric Ndege <ontime.c23@gmail.com>
2024-07-26 22:54:07 +0300

    modified:   server/middleware/auth.js
    	modified:   admin/src/pages/Auth.jsx
    	modified:   admin/src/services/userApi.js
    	modified:   admin/src/util/backendUrl.js
    	modified:   images/cv.pdf
    	modified:   server/controllers/projects.controllers.js
    	modified:   server/controllers/users.controllers.js
    	modified:   server/index.js
    	modified:   server/package.json
    	modified:   server/routes/project.routes.js
    	modified:   server/routes/user.routes.js
    
    	images/Screenshot_2024-07-26_20_43_21.png

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
     const secret = process.env.SECRET;
-    console.log("testing secret", secret);
+    // console.log("testing secret", secret);
     try {
         const token = req.headers.authorization.split(" ")[1] || req.headers.Authorization.split(" ")[1];
         console.log({"Token:": token}, {"Secret:": secret});
